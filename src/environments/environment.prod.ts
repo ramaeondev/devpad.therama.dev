7da91298b4d518931c1bdfe9662db268ba6db49b
ramaeon <Rama.eon@gmail.com>
2025-11-22 23:51:17 +0530

    feat(proxy): implement proxy endpoint with CORS support and authentication

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   supabase: {
-    url: 'https://oktbesfgiyhcjpaljhby.supabase.co',
+    url: 'https://api-devpad.therama.dev',
     anonKey:
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdGJlc2ZnaXloY2pwYWxqaGJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NzAxNDcsImV4cCI6MjA3ODQ0NjE0N30.Hqm4UoWLqwKYO49t5tu5TSft2_oOyvH8DO1Q4wzSKmA',
   },
@@ -11,7 +11,5 @@ export const environment = {
   microsoft: {
     clientId: 'dd8a33f5-bd38-416e-ad5d-042f73a212b8',
     redirectUri: 'https://devpad.therama.dev',
-  },
-  apiUrl: 'https://api.therama.dev',
-  appVersion: '2.0.0',
+  }
 };
