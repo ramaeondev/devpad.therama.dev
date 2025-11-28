b9d2ef101b4e1d0e8a6ab8c4ced4b18d40cfab58
ramaeon <Rama.eon@gmail.com>
2025-11-28 23:46:02 +0530

    feat: implement contact us functionality with a new modal and a Supabase function for email sending via Supabase Edge fucntion and Resend.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  supabase: {
+    url: 'https://api-devpad.therama.dev',
+    anonKey:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdGJlc2ZnaXloY2pwYWxqaGJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI4NzAxNDcsImV4cCI6MjA3ODQ0NjE0N30.Hqm4UoWLqwKYO49t5tu5TSft2_oOyvH8DO1Q4wzSKmA',
+  },
+  google: {
+    clientId: '1054129590650-c4qqjfphqdejcfdcasel8q3a14b5p7kk.apps.googleusercontent.com',
+    apiKey: 'AIzaSyAI6Wn4Ttl_aGjJ6FUEJyppd0kcohxyE9g',
+  },
+  microsoft: {
+    clientId: 'dd8a33f5-bd38-416e-ad5d-042f73a212b8',
+    redirectUri: 'https://devpad.therama.dev',
+  }
+};
